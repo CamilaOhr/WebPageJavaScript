a86fcf973a6713723ff9842217a947a04a85348b
Camila Ortega <m.camila.o1993@gmail.com>
2022-02-16 17:46:30 -0500

    Primer Commit
    Commit con los archivos iniciales

---
+++ main.js
@@ -0,0 +1,4 @@
+const video = document.getElementById("videoBackground");
+const sound = document.getElementById("sound");
+const links = document.querySelectorAll("ul a");
+
---
+++ projectsMain.js
@@ -0,0 +1,395 @@
+
+const proyects = [
+    {
+      id:1,
+      name:"Myosotis",
+      img:"./ProyectImg/InGame.png",
+      proyecType:"VideoGame",
+      description:"aquí iría una descripción corta de lo que serióa el proyecto,  herramientas entre otras cosas",
+      tools: "./ProyectImg/InGame.png",
+      img1: "./ProyectImg/InGame.png",
+      img2: "./ProyectImg/InGame.png",
+      img3: "./ProyectImg/InGame.png",
+      img4: "./ProyectImg/InGame.png",
+      img11: "./ProyectImg/InGame.png",
+      img22: "./ProyectImg/InGame.png",
+      img33: "./ProyectImg/InGame.png",
+      img44: "./ProyectImg/InGame.png"
+
+    },
+    {
+      id:2,
+      name:"Youtube Re-Design",
+      img:"./ProyectImg/InGame.png",
+      proyecType:"UX/UI",
+      description:"aquí iría una descripción corta de lo que serióa el proyecto,  herramientas entre otras cosas",
+      tools: "./ProyectImg/InGame.png",
+      img1: "./ProyectImg/InGame.png",
+      img2: "./ProyectImg/InGame.png",
+      img3: "./ProyectImg/InGame.png",
+      img4: "./ProyectImg/InGame.png",
+      
+      img11: "./ProyectImg/InGame.png",
+      img22: "./ProyectImg/InGame.png",
+      img33: "./ProyectImg/InGame.png",
+      img44: "./ProyectImg/InGame.png"
+    },
+    {
+      id:3,
+      name:"Joula",
+      img:"./ProyectImg/InGame.png",
+      proyecType:"UX/UI",
+      description:"aquí iría una descripción corta de lo que serióa el proyecto,  herramientas entre otras cosas",
+      tools: "./ProyectImg/InGame.png",
+      img1: "./ProyectImg/InGame.png",
+      img2: "./ProyectImg/InGame.png",
+      img3: "./ProyectImg/InGame.png",
+      img4: "./ProyectImg/InGame.png",
+      
+      img11: "./ProyectImg/InGame.png",
+      img22: "./ProyectImg/InGame.png",
+      img33: "./ProyectImg/InGame.png",
+      img44: "./ProyectImg/InGame.png"
+    },
+    {
+      id:4,
+      name:"Cocoon Cup",
+      img:"./ProyectImg/InGame.png",
+      proyecType:"Product Design",
+      description:"aquí iría una descripción corta de lo que serióa el proyecto,  herramientas entre otras cosas",
+      tools: "./ProyectImg/InGame.png",
+      img1: "./ProyectImg/InGame.png",
+      img2: "./ProyectImg/InGame.png",
+      img3: "./ProyectImg/InGame.png",
+      img4: "./ProyectImg/InGame.png",
+      
+      img11: "./ProyectImg/InGame.png",
+      img22: "./ProyectImg/InGame.png",
+      img33: "./ProyectImg/InGame.png",
+      img44: "./ProyectImg/InGame.png"
+    },
+    {
+      id:5,
+      name:"Xian Quest",
+      img:"./ProyectImg/InGame.png",
+      proyecType:"VideoGame",
+      description:"aquí iría una descripción corta de lo que serióa el proyecto,  herramientas entre otras cosas",
+      tools: "./ProyectImg/InGame.png",
+      img1: "./ProyectImg/InGame.png",
+      img2: "./ProyectImg/InGame.png",
+      img3: "./ProyectImg/InGame.png",
+      img4: "./ProyectImg/InGame.png",
+      
+      img11: "./ProyectImg/InGame.png",
+      img22: "./ProyectImg/InGame.png",
+      img33: "./ProyectImg/InGame.png",
+      img44: "./ProyectImg/InGame.png"
+    },
+    {
+      id:6,
+      name:"Psychopomp",
+      img:"./ProyectImg/InGame.png",
+      proyecType:"VideoGame",
+      description:"aquí iría una descripción corta de lo que serióa el proyecto,  herramientas entre otras cosas",
+      tools: "./ProyectImg/InGame.png",
+      img1: "./ProyectImg/InGame.png",
+      img2: "./ProyectImg/InGame.png",
+      img3: "./ProyectImg/InGame.png",
+      img4: "./ProyectImg/InGame.png",
+      
+      img11: "./ProyectImg/InGame.png",
+      img22: "./ProyectImg/InGame.png",
+      img33: "./ProyectImg/InGame.png",
+      img44: "./ProyectImg/InGame.png"
+    },
+    {
+      id:7,
+      name:"Elba & Llenita",
+      img:"./ProyectImg/InGame.png",
+      proyecType:"VideoGame",
+      description:"aquí iría una descripción corta de lo que serióa el proyecto,  herramientas entre otras cosas",
+      tools: "./ProyectImg/InGame.png",
+      img1: "./ProyectImg/InGame.png",
+      img2: "./ProyectImg/InGame.png",
+      img3: "./ProyectImg/InGame.png",
+      img4: "./ProyectImg/InGame.png",
+      
+      img11: "./ProyectImg/InGame.png",
+      img22: "./ProyectImg/InGame.png",
+      img33: "./ProyectImg/InGame.png",
+      img44: "./ProyectImg/InGame.png"
+    },
+    {
+      id:8,
+      name:"Environment House",
+      img:"./ProyectImg/InGame.png",
+      proyecType:"3D Model",
+      description:"aquí iría una descripción corta de lo que serióa el proyecto,  herramientas entre otras cosas",
+      tools: "./ProyectImg/InGame.png",
+      img1: "./ProyectImg/InGame.png",
+      img2: "./ProyectImg/InGame.png",
+      img3: "./ProyectImg/InGame.png",
+      img4: "./ProyectImg/InGame.png",
+      
+      img11: "./ProyectImg/InGame.png",
+      img22: "./ProyectImg/InGame.png",
+      img33: "./ProyectImg/InGame.png",
+      img44: "./ProyectImg/InGame.png"
+    },
+    {
+      id:9,
+      name:"Creature Model",
+      img:"./ProyectImg/InGame.png",
+      proyecType:"3D Model",
+      description:"aquí iría una descripción corta de lo que serióa el proyecto,  herramientas entre otras cosas",
+      tools: "./ProyectImg/InGame.png",
+      img1: "./ProyectImg/InGame.png",
+      img2: "./ProyectImg/InGame.png",
+      img3: "./ProyectImg/InGame.png",
+      img4: "./ProyectImg/InGame.png",
+      
+      img11: "./ProyectImg/InGame.png",
+      img22: "./ProyectImg/InGame.png",
+      img33: "./ProyectImg/InGame.png",
+      img44: "./ProyectImg/InGame.png"
+    }
+    ]
+    
+    
+    const proyectsContainer = document.querySelector(".Proyect__Container");
+    const proyectFilter = document.querySelector(".Proyect__Filter");
+    const proyectselected= document.querySelector(".proyect__selected");
+    const favoriteArray =[];
+    
+    
+    
+    const btnFilterVideoGame =document.createElement("button");
+    btnFilterVideoGame.classList.add("btn-filter");
+    btnFilterVideoGame.textContent ="Video Games";
+    btnFilterVideoGame.onclick = () => {filterProyects("VideoGame")};
+    const btnFilterUXUI =document.createElement("button");
+    btnFilterUXUI.classList.add("btn-filter");
+    btnFilterUXUI.textContent ="UX/UI";
+    btnFilterUXUI.onclick = () => {filterProyects("UX/UI")};
+    const btnFilterProduct =document.createElement("button");
+    const btnFilter3d =document.createElement("button");
+    btnFilter3d.classList.add("btn-filter");
+    btnFilter3d.textContent ="3D Model";
+    btnFilter3d.onclick = () => { filterProyects("3D Model")};
+    const btnFilterAll =document.createElement("button");
+    btnFilterAll.classList.add("btn-filter");
+    btnFilterAll.textContent ="All";
+    btnFilterAll.onclick = () => { mostrarProyectos(proyects)};
+
+    
+    btnFilterProduct.classList.add("btn-filter");
+    btnFilterProduct.textContent ="Favorites";
+    btnFilterProduct.onclick = () => { filterFavorites(favoriteArray)};
+    
+     
+    
+    proyectFilter.append(btnFilterVideoGame)
+    proyectFilter.append(btnFilterUXUI)
+    proyectFilter.append(btnFilterProduct)
+    proyectFilter.append(btnFilter3d)
+    proyectFilter.append(btnFilterAll)
+    
+    recoverFavorites();
+    mostrarProyectos(proyects);
+    
+    
+    
+    function mostrarProyectos(proyectos){
+      proyectsContainer.innerHTML="";
+      
+      //Scripting
+        proyectos.forEach(function(proyect){
+            const divProyect = document.createElement("div");
+            divProyect.classList.add("card");
+            divProyect.onclick = () => {selectProyect(proyect.id)};
+            
+            const imageProyect =document.createElement("img");
+            imageProyect.classList.add("imageProyect");
+            imageProyect.src= proyect.img
+    
+            const titleProyect = document.createElement("h3");
+            titleProyect.textContent = proyect.name;
+    
+           
+            divProyect.appendChild(titleProyect);
+            divProyect.appendChild(imageProyect);
+             
+            proyectsContainer.appendChild(divProyect);
+        })
+    }
+    
+    function selectProyect(id){
+      const proyectToShow = proyects.find(proyect => proyect.id === id);
+       showProyectSide(proyectToShow);
+    }
+
+    function createDiv(className){
+      const divProyectfull = document.createElement("div");
+      divProyectfull.classList.add(className);
+      return divProyectfull;
+    }
+
+    function createImg(url){
+      const projectCarrouselImage = document.createElement("img");
+      projectCarrouselImage.classList.add("carrouselImages");
+      projectCarrouselImage.src= url;
+      return projectCarrouselImage;
+    }
+
+    function createImgDisplay(url){
+      const projectCarrouselImage = document.createElement("img");
+      projectCarrouselImage.classList.add("displaylImages");
+      projectCarrouselImage.src= url;
+      return projectCarrouselImage;
+    }
+
+
+    
+    function showProyectSide (shown){
+    
+      proyectselected.innerHTML="";
+        const divProyectFull = createDiv("cardFull");
+        
+        const divProjectInfo = createDiv("product__content");
+
+        const divProjectimgs = createDiv("product__imgs");
+
+        const divImgDisplay = createDiv("img__display");
+
+        const divImgShowcase = createDiv("img__showcase");
+       
+   
+        const titleProyect = document.createElement("h3");
+        titleProyect.textContent = shown.name;
+
+        const descriptionProject = document.createElement("p");
+        descriptionProject.textContent= shown.description;
+        descriptionProject.classList.add("project__detail");
+
+        const projectTools = document.createElement("img");
+        projectTools.classList.add("toolImage");
+        projectTools.src=shown.tools;
+
+        const btnFavorito = document.createElement("button");
+        btnFavorito.classList.add("btn-favorito");
+        btnFavorito.textContent = "Agregar a Favoritos";
+        btnFavorito.onclick = () => {agregarFavorito (shown.id)};
+
+        //Carrousel
+
+        const divCarrousel = createDiv("carrousel");
+
+        const projectReferenceImages = createImg (shown.img1);
+        
+        const projectReferenceImages2 = createImg(shown.img2);
+        
+        const projectReferenceImages3 = createImg(shown.img3);
+        
+        const projectReferenceImages4 = createImg(shown.img4);
+
+        //Display
+
+        const projectReferenceImagesDisplay = createImgDisplay(shown.img11);
+        
+        const projectReferenceImagesDisplay2 = createImgDisplay(shown.img22);;
+        
+        const projectReferenceImagesDisplay3 = createImgDisplay(shown.img33);;
+        
+        const projectReferenceImagesDisplay4 = createImgDisplay(shown.img44);
+
+
+       
+
+        divProjectInfo.appendChild(titleProyect);
+        divProjectInfo.appendChild(descriptionProject);
+        divProjectInfo.appendChild(btnFavorito);
+        
+        
+         
+        divProjectimgs.appendChild(divImgDisplay);
+        divImgDisplay.appendChild(divImgShowcase);
+        divImgDisplay.appendChild(divCarrousel);
+        
+        divImgShowcase.appendChild(projectReferenceImagesDisplay);
+        divImgShowcase.appendChild(projectReferenceImagesDisplay2);
+        divImgShowcase.appendChild(projectReferenceImagesDisplay3);
+        divImgShowcase.appendChild(projectReferenceImagesDisplay4);
+
+        divCarrousel.appendChild(projectReferenceImages);
+        divCarrousel.appendChild(projectReferenceImages2);
+        divCarrousel.appendChild(projectReferenceImages3);
+        divCarrousel.appendChild(projectReferenceImages4);
+
+        
+        divProyectFull.appendChild(divProjectInfo);
+        divProyectFull.appendChild(divProjectimgs);
+        
+                   
+        proyectselected.appendChild(divProyectFull);
+     }
+    
+    
+    function filterProyects(proyectTypes){
+      console.log("hola desde botón");
+      const filterForProyect = proyects.filter(proyects => proyects.proyecType === proyectTypes);
+      console.log(filterForProyect);
+      mostrarProyectos(filterForProyect);
+    
+    }
+
+    function agregarFavorito(id){
+      const favoriteproject = proyects.find(shown => shown.id === id);
+      
+
+      if (favoriteArray.includes(favoriteproject) === false) favoriteArray.push(favoriteproject);
+
+
+      filterFavorites(favoriteArray);
+
+      localStorage.setItem("favoritos", JSON.stringify(favoriteArray));
+
+      
+    }
+
+    function filterFavorites(favorites){
+      proyectsContainer.innerHTML="";
+      
+      //Scripting
+        favorites.forEach(function(favorite){
+            const divProyect = document.createElement("div");
+            divProyect.classList.add("card");
+            divProyect.onclick = () => {selectProyect(favorite.id)};
+            
+            const imageProyect =document.createElement("img");
+            imageProyect.classList.add("imageProyect");
+            imageProyect.src= favorite.img
+    
+            const titleProyect = document.createElement("h3");
+            titleProyect.textContent = favorite.name;
+    
+           
+            divProyect.appendChild(titleProyect);
+            divProyect.appendChild(imageProyect);
+             
+            proyectsContainer.appendChild(divProyect);
+        })
+
+    }
+
+    function recoverFavorites(){
+      let recover = JSON.parse(localStorage.getItem("favoritos"));
+      console.log(recover);
+
+      if(recover){
+        recover.forEach(element =>{
+          agregarFavorito(element.id)
+        });
+      }
+    }
+    
+    
